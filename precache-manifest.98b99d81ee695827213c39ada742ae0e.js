cff2714832ab855b77f7592bf729a3100a00d256
Malcolm777 <69059606+Malcolm777@users.noreply.github.com>
2022-08-15 17:46:30 -0500

    Updates

---
+++ precache-manifest.98b99d81ee695827213c39ada742ae0e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "eddefea6e99fce116b43",
+    "url": "/Portfolio-Website/static/css/main.69b9f78b.chunk.css"
+  },
+  {
+    "revision": "eddefea6e99fce116b43",
+    "url": "/Portfolio-Website/static/js/main.eddefea6.chunk.js"
+  },
+  {
+    "revision": "65407b2d05b9c494f006",
+    "url": "/Portfolio-Website/static/js/1.65407b2d.chunk.js"
+  },
+  {
+    "revision": "9b6b17faf27c71f0d4ca",
+    "url": "/Portfolio-Website/static/js/runtime~main.9b6b17fa.js"
+  },
+  {
+    "revision": "160b9bff2b00aaf893bcdb10ffdf863c",
+    "url": "/Portfolio-Website/static/media/workImg.160b9bff.jpeg"
+  },
+  {
+    "revision": "12fd0ac1b291d6c50739e55c389a1659",
+    "url": "/Portfolio-Website/static/media/eComm.12fd0ac1.jpeg"
+  },
+  {
+    "revision": "ead892939b6efed56fdae86db81fb4fb",
+    "url": "/Portfolio-Website/static/media/stockViz.ead89293.png"
+  },
+  {
+    "revision": "dbe3338f1a1f12564c2ff2746ee938df",
+    "url": "/Portfolio-Website/static/media/tradingAlgo.dbe3338f.jpeg"
+  },
+  {
+    "revision": "5d150ed22ee7d842de27ca4b959f1a26",
+    "url": "/Portfolio-Website/static/media/electricCar.5d150ed2.jpeg"
+  },
+  {
+    "revision": "098f37ba6cb9a57f90caaae1b3f0b0a6",
+    "url": "/Portfolio-Website/static/media/JRE.098f37ba.jpeg"
+  },
+  {
+    "revision": "072243bef67613c843fb4a572cb0fd3a",
+    "url": "/Portfolio-Website/index.html"
+  }
+];
