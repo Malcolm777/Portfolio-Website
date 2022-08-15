7baa903501d4260f22a273a36d54d0495e904cb9
Malcolm777 <69059606+Malcolm777@users.noreply.github.com>
2022-08-14 23:44:51 -0500

    Trigger rebuild

---
+++ src/components/Portfolio.js
@@ -0,0 +1,208 @@
+import React, { Component } from 'react';
+import WorkImg from './images/workImg.jpeg';
+import eComm from './images/eComm.jpeg';
+import stockViz from './images/stockViz.png'; 
+import tradingAlgo from './images/tradingAlgo.jpeg';
+import electricCar from './images/electricCar.jpeg'; 
+import JRE from './images/JRE.jpeg'; 
+
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Works.</h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div name = 'work' className = 'w-full m:h-screen text-gray-300 bg-[#0a192f]'>
+        <div className = 'max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className = 'pb-8'>
+                <p className = 'text-4xl font-bold inline border-b-4 text-pink 300 border-pink-600'>Click on an image below to check out the project!</p>
+                
+            </div>
+
+            <div 
+            className = 'grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+                {/*grid items */}
+                <div 
+                style = {{backgroundImage:`url(${eComm})`}}
+                className = 'shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/*Hover effects*/}
+                    <div className = 'opacity-0 group-hover:opacity-100'> 
+                        <span className = 'text-2xl font-bold text-pink tracking-wider'>
+                            React JS Application 
+
+                        </span>
+                        <div className = 'pt-8 text-center'>
+                        <a href = "https://9eaf-2601-2c6-5000-7fb0-8956-abf1-7e22-2c47.ngrok.io">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-pink-600 font-bold text-lg'>
+                                    Demo 
+                                </button>
+                        </a>
+                        <a href = "https://github.com/Malcolm777/nexus-clothing " target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-pink-600 font-bold text-lg'>
+                                    Code
+                                </button>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+                {/*grid items */}
+                <div 
+                style = {{backgroundImage:`url(${stockViz})`}}
+                className = 'shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/*Hover effects*/}
+                    <div className = 'opacity-0 group-hover:opacity-100'> 
+                        <span className = 'text-2xl font-bold text-white tracking-wider'>
+                            HTML5/CSS3 website 
+
+                        </span>
+                        <div className = 'pt-8 text-center'>
+                        <a href = "https://malcolm777.github.io/Elite-Capital-MOCK-/" target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Demo 
+                                </button>
+                        </a>
+                        <a href = "https://github.com/Malcolm777/Elite-Capital-MOCK-" target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Code
+                                </button>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+                {/*grid items */}
+                <div 
+                style = {{backgroundImage:`url(${tradingAlgo})`}}
+                className = 'shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/*Hover effects*/} 
+                    <div className = 'opacity-0 group-hover:opacity-100'> 
+                        <span className = ' text-2xl font-bold text-white tracking-wider'>
+                            Python, pandas, numPy application  
+
+                        </span>
+                        <div className = 'pt-8 text-center'>
+                        <a href = "https://github.com/Malcolm777/Trading_algorithms/blob/master/AAPL_MACD_8_26_20.ipynb" target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Demo 
+                                </button>
+                        </a>
+                        <a href = "https://github.com/Malcolm777/Trading_algorithms" target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Code
+                                </button>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+                {/*grid items */}
+                <div 
+                style = {{backgroundImage:`url(${electricCar})`}}
+                className = 'shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/*Hover effects*/}
+                    <div className = 'opacity-0 group-hover:opacity-100'> 
+                        <span className = 'text-2xl font-bold text-white tracking-wider'>
+                            Java Application 
+
+                        </span>
+                        <div className = 'pt-8 text-center'>
+                        <a href = "https://github.com/Malcolm777/Electric-Car-Shop/tree/main" target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Demo 
+                                </button>
+                        </a>
+                       <a href = "https://github.com/Malcolm777/Electric-Car-Shop/tree/main" target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Code
+                                </button>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+                {/*grid items */}
+                <div 
+                style = {{backgroundImage:`url(${JRE})`}}
+                className = 'shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/*Hover effects*/}
+                    <div className = 'opacity-0 group-hover:opacity-100'> 
+                        <span className = 'text-2xl font-bold text-white tracking-wider'>
+                            Java with Spring/Hibernate framework
+
+                        </span>
+                        <div className = 'pt-8 text-center'>
+                        <a href = "https://github.com/Malcolm777/StudentDirectory" target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Demo 
+                                </button>
+                        </a>
+                        <a href = "https://github.com/Malcolm777/StudentDirectory" target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Code
+                                </button>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+                {/*grid items */}
+                <div 
+                style = {{backgroundImage:`url(${WorkImg})`}}
+                className = 'shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/*Hover effects*/}
+                    <div className = 'opacity-0 group-hover:opacity-100'> 
+                        <span className = 'text-2xl font-bold text-white tracking-wider'>
+                            Java Data Structures
+
+                        </span>
+                        <div className = 'pt-8 text-center'>
+                        <a href = "https://github.com/Malcolm777/CitiesDataStructures" target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Demo 
+                                </button>
+                        </a>
+                        <a href = "https://github.com/Malcolm777/CitiesDataStructures" target = "_blank">
+                                <button className = 'text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Code
+                                </button>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+                /* 
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                */
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,120 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Malcolm Gilbert",
+    "role": "Application Engineer ",
+    "linkedinId":"",
+    "skypeid": "Your skypeid",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/malcolm-gilbert-b0a820149/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Malcolm777",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/MalcolmInTheM",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I'm a senior computer science student at the University of Maryland, Baltimore County. At university, I've completed projects in C++, Java, and Python. This past summer 2022, I interned at AIG as a technology analyst intern, working on the application security team. I also supported a full-stack engineer with defect fixes and creating RESTful applications in Java, Spring, and SQL/PostgreSQL. Currently, I'm in my last semester virtually, on a half-time schedule, so I'm searching for back-end dev and/or full-stack dev opportunities. I love to automate and create products that make life easier.",
+    "address":"Washington, D.C. | Houston, Texas",
+    "website":"https://www.linkedin.com/in/malcolm-gilbert-b0a820149/",
+    "education":[
+      {
+        "UniversityName":"University of Maryland, Global Campus",
+        "specialization":"Computer Science, B.S.",
+        "MonthOfPassing":"Dec 2020 - ",
+        "YearOfPassing":"Dec 2022",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"University of Maryland, Baltimore County",
+        "specialization":"Visual Arts, B.A.",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2018 - May 2022",
+        "Achievements":"Achievements: 3x Marine Scholar (Marine Corps Scholarship Foundation), NSBE Member, Vice President - Investing & Trading Club" 
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"AIG",
+        "specialization":"Technology Analyst Intern",
+        "MonthOfLeaving":"Jun",
+        "YearOfLeaving":"2022 - Aug 2022",
+        "Achievements":"Implemented Power Query functions for PowerBI to assist in gathering, processing, analyzing, and presenting application security test metrics through a data visualization board; Supported full-stack application engineer with defect fixes, making requests to call API endpoints in Java and Spring, and SQL database updates to link serializable tokens for in-progress and fully committed insurance products; Re-imaged computers with BitLocker hardware encryption for cyber threat management lab to perform security risk reduction assessments and security penetration testing; Fixed Java and Spring code vulnerabilities such as cross-site scripting, DOM manipulation, XML external entities, to complete 40 hours of developer’s security training"
+      },
+      {
+        "CompanyName":"ESPN+",
+        "specialization":"TV Production Intern",
+        "MonthOfLeaving":"Nov 2019 - ",
+        "YearOfLeaving":"March 2020",
+        "Achievements":"TV Sports Broadcasting Intern (Nov 2019 – March 2020); Provided motion graphics producer with numbered codes which correspond to specific statistic data from excel spreadsheets; Created comprehensive Excel spreadsheets for player statistics aired on live television; Communicated between producer, technical director, replay specialists, motion graphic producer, and in-house statisticians to provide live on-air graphics" 
+      }, 
+      {
+        "CompanyName":"Elite Capital Association, LLC.",
+        "specialization":"Data Analytics Intern",
+        "MonthOfLeaving":"Feb 2021 - Current", 
+        "YearOfLeaving": "", 
+        "Achievements": "Elite Capital Association, LLC; Investment consultant (January 2021-current); Create trading algorithms in Python’s Jupyter Notebook with Pandas and Numpy to generate investment ideas for a Discord with 400 subscribed members; Designed a mock website in HTML5, CSS3, JavaScript ES6 with REACT library to build the user interface; Provide investment guidance to members through automated stock option contract buy and sell signals" 
+
+      }
+    ],
+    "skillsDescription":"Java, JavaScript, HTML5, CSS3, BootStrap, SQL, PostgreSQL, AWS Certified Cloud Practitioner",
+    /*"skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"ReactJS"
+      },
+      {
+        "skillname":"Java"
+      }
+    ]*/
+    "portfolio":[
+      {
+        "name":"Elite Capital Mock Website",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/EliteMock.jpg", 
+        "website": "https://malcolm777.github.io/Elite-Capital-MOCK-/"
+      }
+      /*
+      {
+        "name":"Electric Car Rental System",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"E-commerce Website",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"Java Data Structures",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+      */
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Technical guy"
+      },
+      {
+        "description":"Testimonial Sample",
+        "name":"Some profound technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
