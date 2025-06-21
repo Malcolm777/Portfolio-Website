7737f29369bdc5276e9f35daae332a43e008ed88
Malcolm777 <69059606+Malcolm777@users.noreply.github.com>
2025-06-21 15:57:57 -0400

    Update resumeData.js

--- src/resumeData.js
+++ src/resumeData.js
@@ -21,7 +21,7 @@ let resumeData = {
           "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"Malcolm Gilbert is a software engineer, with 2 years of experience in the financial services industry. In Wealth and Investment Management, Malcolm has created features for brokerage, investment, and wealth products for Wells Fargo Advisors and the Private Bank, writing code in Java, TypeScript, and React.",
+    "aboutme":" Malcolm Gilbert is a software engineer, with 2 years of experience in the financial services industry. In Wealth and Investment Management, Malcolm has created features for brokerage, investment, and wealth products for Wells Fargo Advisors and the Private Bank, writing code in Java, TypeScript, and React.",
     "address":"Charlotte, NC",
     "website":"https://www.linkedin.com/in/malcolm-gilbert-b0a820149/",
     "education":[
